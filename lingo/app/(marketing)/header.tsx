067bdf67d8ad6e2db3483375bb9345f1b9409152
Nélio Dias <jrneliodias@gmail.com>
2024-03-18 00:36:30 -0300

    feat: create the singin and singup modules

--- lingo/app/(marketing)/header.tsx
+++ lingo/app/(marketing)/header.tsx
@@ -1,9 +1,36 @@
+
+import { Button } from '@/components/ui/button'
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { Ghost, Loader } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 export const Header = () => {
     return (
         <header className='h-20 w-full border-b-2 border-slate-200 px-4'>
-            Header
+            <div className="lg:max-w-screen-lg mx-auto flex items-center h-full justify-between">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src="/mascot.svg" height={40} width={40} alt='mascot' />
+                    <h1 className='text-2xl font-extrabold text-green-600 tracking-wide'>
+                        Lingo
+                    </h1>
+                </div>
+                <ClerkLoading>
+                    <Loader className='h-5  w-5 text-muted-foreground animate-spin' />
+                </ClerkLoading>
+                <ClerkLoaded>
+
+                    <SignedIn>
+                        <UserButton afterSignOutUrl='/' />
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton mode="modal" afterSignInUrl='/learn' afterSignUpUrl='/learn'>
+                            <Button size={"lg"} variant={"ghost"}> login</Button>
+                        </SignInButton>
+
+                    </SignedOut>
+                </ClerkLoaded>
+            </div>
         </header>
     )
 }
--- lingo/app/(marketing)/page.tsx
+++ lingo/app/(marketing)/page.tsx
@@ -1,13 +1,55 @@
 import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignIn, SignInButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <div className="text-green-500 text-3xl font-bold">
-        Hello Lingo
+      <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+        <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+          <Image src={'/hero.svg'} fill alt="hero" />
+        </div>
+        <div className="flex flex-col items-center gap-y-8">
+          <h1 className="text-xl  lg:text-3xl font-bold ">
+            Learn, practice, and master new languages with Lingo.
+          </h1>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn">
+                <Button size={"lg"} variant={'secondary'} className="w-full">
+                  Get started
+                </Button>
+
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn">
+                <Button size={"lg"} variant={'primaryOutline'} className="w-full">
+                  I already have an account
+                </Button>
+
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size={'lg'} variant={'secondary'} className="w-full" asChild>
+                <Link href={'/learn'}>
+                  Continue Learning
+                </Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
       </div>
 
-      <Button>Click me </Button>
     </div>
   );
 }
