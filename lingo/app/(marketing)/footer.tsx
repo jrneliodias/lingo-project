735874f6132e3e4504fb6ea2838432c34d70b85b
Nélio Dias <jrneliodias@gmail.com>
2024-03-18 01:02:51 -0300

    feat: style the footer

--- lingo/app/(marketing)/footer.tsx
+++ lingo/app/(marketing)/footer.tsx
@@ -1,9 +1,32 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 export const Footer = () => {
     return (
         <footer className='hidden lg:block border-t-2 p-2 border-slate-200 h-20 w-full'>
-            footer
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button size={'lg'} variant={'ghost'} className='w-full'>
+                    <Image src={"/es.svg"} alt='Spain' height={32} width={40} className='mr-4 rounded-md' />
+                    Spain
+                </Button>
+                <Button size={'lg'} variant={'ghost'} className='w-full'>
+                    <Image src={"/fr.svg"} alt='France' height={32} width={40} className='mr-4 rounded-md' />
+                    France
+                </Button>
+                <Button size={'lg'} variant={'ghost'} className='w-full'>
+                    <Image src={"/it.svg"} alt='Italian' height={32} width={40} className='mr-4 rounded-md' />
+                    Italian
+                </Button>
+                <Button size={'lg'} variant={'ghost'} className='w-full'>
+                    <Image src={"/jp.svg"} alt='Japonese' height={32} width={40} className='mr-4 rounded-md' />
+                    Japan
+                </Button>
+                <Button size={'lg'} variant={'ghost'} className='w-full'>
+                    <Image src={"/hr.svg"} alt='Croatian' height={32} width={40} className='mr-4 rounded-md' />
+                    Croatian
+                </Button>
+            </div>
         </footer>
     )
 }
--- lingo/app/(marketing)/page.tsx
+++ lingo/app/(marketing)/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { ClerkLoaded, ClerkLoading, SignIn, SignInButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import { Loader } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,41 +12,43 @@ export default function Home() {
           <Image src={'/hero.svg'} fill alt="hero" />
         </div>
         <div className="flex flex-col items-center gap-y-8">
-          <h1 className="text-xl  lg:text-3xl font-bold ">
+          <h1 className="text-xl  lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
             Learn, practice, and master new languages with Lingo.
           </h1>
-          <ClerkLoading>
-            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-          </ClerkLoading>
-          <ClerkLoaded>
-            <SignedOut>
-              <SignUpButton
-                mode="modal"
-                afterSignInUrl="/learn"
-                afterSignUpUrl="/learn">
-                <Button size={"lg"} variant={'secondary'} className="w-full">
-                  Get started
-                </Button>
+          <div className="flex flex-col items-center gap-y-3 min-w-[330px] w-full">
+            <ClerkLoading>
+              <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+            </ClerkLoading>
+            <ClerkLoaded>
+              <SignedOut>
+                <SignUpButton
+                  mode="modal"
+                  afterSignInUrl="/learn"
+                  afterSignUpUrl="/learn">
+                  <Button size={"lg"} variant={'secondary'} className="w-full">
+                    Get started
+                  </Button>
 
-              </SignUpButton>
-              <SignInButton
-                mode="modal"
-                afterSignInUrl="/learn"
-                afterSignUpUrl="/learn">
-                <Button size={"lg"} variant={'primaryOutline'} className="w-full">
-                  I already have an account
-                </Button>
+                </SignUpButton>
+                <SignInButton
+                  mode="modal"
+                  afterSignInUrl="/learn"
+                  afterSignUpUrl="/learn">
+                  <Button size={"lg"} variant={'primaryOutline'} className="w-full">
+                    I already have an account
+                  </Button>
 
-              </SignInButton>
-            </SignedOut>
-            <SignedIn>
-              <Button size={'lg'} variant={'secondary'} className="w-full" asChild>
-                <Link href={'/learn'}>
-                  Continue Learning
-                </Link>
-              </Button>
-            </SignedIn>
-          </ClerkLoaded>
+                </SignInButton>
+              </SignedOut>
+              <SignedIn>
+                <Button size={'lg'} variant={'secondary'} className="w-full" asChild>
+                  <Link href={'/learn'}>
+                    Continue Learning
+                  </Link>
+                </Button>
+              </SignedIn>
+            </ClerkLoaded>
+          </div>
         </div>
       </div>
 
