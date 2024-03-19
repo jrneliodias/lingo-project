f9bf285b647ffddae5b051de3d6a9f4f938badf3
Nélio Dias <jrneliodias@gmail.com>
2024-03-19 01:44:51 -0300

    feat: create the feed wrapper and his header

--- lingo/app/(main)/learn/page.tsx
+++ lingo/app/(main)/learn/page.tsx
@@ -1,8 +1,27 @@
+import StickWrapper from '@/components/StickWrapper'
+import FeedWrapper from '@/components/feed-wrapper'
 import React from 'react'
+import { Header } from './header'
+import UserProgress from '@/components/user-progress'
 
 const LearnPage = () => {
     return (
-        <div>LearnPage</div>
+        <div className='flex flex-row-reverse gap-[48px] px-6'>
+            <StickWrapper>
+                <UserProgress
+                    activeCourse={{ title: "Spanish", imageSrc: "es.svg" }}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false} />
+            </StickWrapper>
+            <FeedWrapper>
+                <Header title={'Spanish'} />
+                <div className="space-y-4">
+
+
+                </div>
+            </FeedWrapper>
+        </div >
     )
 }
 
---
+++ lingo/components/StickWrapper.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode;
+}
+const StickWrapper = ({ children }: Props) => {
+    return (<div>
+
+        <div className='hidden lg:block w-[368px] sticky self-end bottom-6'>
+            <div className="min-h-[calc(100vh-48px)] sticky top-6 flex flex-col gap-y-4">
+                {children}
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default StickWrapper
