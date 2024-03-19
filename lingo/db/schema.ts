7bf289a228f1f559089c815e94e3642e22e3824d
Nélio Dias <jrneliodias@gmail.com>
2024-03-19 19:52:35 -0300

    feat: setup drizzle orm

---
+++ lingo/db/schema.ts
@@ -0,0 +1,7 @@
+import { serial, pgTable, text } from "drizzle-orm/pg-core";
+
+export const courses = pgTable("courses", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  imageSrc: text("image_src").notNull(),
+});
---
+++ lingo/drizzle.config.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./db/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DRIZZLE_DATABASE_URL!,
+  },
+} satisfies Config;
