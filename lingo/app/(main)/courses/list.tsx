8f9db1ae3bec8ca717e433e404cc7134b4d08bee
Nélio Dias <jrneliodias@gmail.com>
2024-03-21 00:26:23 -0300

    feat: implement the transaction in courses

--- lingo/app/(main)/courses/list.tsx
+++ lingo/app/(main)/courses/list.tsx
@@ -1,14 +1,35 @@
 "use client"
 
 import { courses, userProgress } from '@/db/schema'
-import React from 'react'
+import React, { useTransition } from 'react'
 import Card from './card';
+import { useRouter } from 'next/navigation';
+import { upsertUserProgress } from '@/actions/user-progress';
+import { toast } from "sonner"
+
 type Props = {
     courses: typeof courses.$inferSelect[];
     activeCourseId: typeof userProgress.$inferSelect.activeCourseId;
 }
 
 const List = ({ courses, activeCourseId }: Props) => {
+
+    const router = useRouter()
+    const [pending, startTransition] = useTransition()
+
+    const onClick = (id: number) => {
+        if (pending) return;
+
+        if (id === activeCourseId) {
+            return router.push("/learn")
+        }
+
+        startTransition(() => {
+            upsertUserProgress(id)
+                .catch(() => toast.error("Something went wrong."))
+        })
+
+    }
     return (
         <div className='pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4'>
             {courses.map((course) => (
@@ -17,9 +38,9 @@ const List = ({ courses, activeCourseId }: Props) => {
                     id={course.id}
                     key={course.id}
                     imageSrc={course.imageSrc}
-                    onClick={() => { }}
+                    onClick={onClick}
                     active={course.id === activeCourseId}
-                    disabled={false}
+                    disabled={pending}
                 />
             ))}
         </div>
--- lingo/app/(main)/learn/page.tsx
+++ lingo/app/(main)/learn/page.tsx
@@ -1,10 +1,21 @@
+import { getUserProgress } from '@/db/queries'
 import StickWrapper from '@/components/stick-wrapper'
 import FeedWrapper from '@/components/feed-wrapper'
 import React from 'react'
 import { Header } from './header'
 import UserProgress from '@/components/user-progress'
+import { redirect } from 'next/navigation'
 
-const LearnPage = () => {
+
+const LearnPage = async () => {
+
+    const userProgressData = getUserProgress()
+
+    const [userProgress] = await Promise.all([userProgressData])
+
+    if (!userProgress || !userProgress.activeCourse) {
+        redirect("/courses")
+    }
     return (
         <div className='flex flex-row-reverse gap-[48px] px-6'>
             <StickWrapper>
