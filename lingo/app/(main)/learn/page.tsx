039bfb776c5e49656fe81b203db2179f2c7b1610
Nélio Dias <jrneliodias@gmail.com>
2024-03-21 23:59:05 -0300

    feat: update the learn page with user progress from database

--- lingo/app/(main)/learn/page.tsx
+++ lingo/app/(main)/learn/page.tsx
@@ -20,13 +20,13 @@ const LearnPage = async () => {
         <div className='flex flex-row-reverse gap-[48px] px-6'>
             <StickWrapper>
                 <UserProgress
-                    activeCourse={{ title: "Spanish", imageSrc: "es.svg" }}
-                    hearts={5}
-                    points={100}
+                    activeCourse={userProgress.activeCourse}
+                    hearts={userProgress.hearts}
+                    points={userProgress.points}
                     hasActiveSubscription={false} />
             </StickWrapper>
             <FeedWrapper>
-                <Header title={'Spanish'} />
+                <Header title={userProgress.activeCourse.title} />
                 <div className="space-y-4">
 
 
