3f00eb577d8ac550d356cd5174621b6ca91c4f71
Nélio Dias <jrneliodias@gmail.com>
2024-03-21 23:50:33 -0300

    core: create seeds.ts to database

---
+++ lingo/scripts/seed.ts
@@ -0,0 +1,43 @@
+import { neon } from "@neondatabase/serverless";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log("Seeding database");
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: "Spanish",
+        imageSrc: "/es.svg",
+      },
+      {
+        id: 2,
+        title: "Italian",
+        imageSrc: "/it.svg",
+      },
+      {
+        id: 3,
+        title: "Freach",
+        imageSrc: "/fr.svg",
+      },
+      {
+        id: 4,
+        title: "Croatian",
+        imageSrc: "/hr.svg",
+      },
+    ]);
+    console.log("Seeding finished.");
+  } catch (error) {
+    throw new Error("Failed to seed the database");
+  }
+};
+
+main();
