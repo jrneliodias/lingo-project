9ddbf2138f2ed086cc6c2e8b82744b5d6948548d
Nélio Dias <jrneliodias@gmail.com>
2024-03-23 16:58:13 -0300

    feat: insert the unit in the learn page

--- lingo/app/(main)/learn/page.tsx
+++ lingo/app/(main)/learn/page.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { Header } from './header'
 import UserProgress from '@/components/user-progress'
 import { redirect } from 'next/navigation'
+import Unit from './unit'
 
 
 const LearnPage = async () => {
@@ -30,8 +31,15 @@ const LearnPage = async () => {
                 <Header title={userProgress.activeCourse.title} />
                 {units.map((unit) => (
 
-                    <div key={unit.id} className="space-y-4">
-                        {JSON.stringify(unit)}
+                    <div key={unit.id} className="mb-10">
+                        <Unit
+                            id={unit.id}
+                            order={unit.order}
+                            description={unit.description}
+                            title={unit.title}
+                            lessons={unit.lessons}
+                            activeLesson={undefined}
+                            activeLessonPercentage={0}></Unit>
                     </div>
                 ))}
 
