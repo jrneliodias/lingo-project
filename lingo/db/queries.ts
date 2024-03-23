878a8b09887610749b2c4a3fe55379c6a9a64496
Nélio Dias <jrneliodias@gmail.com>
2024-03-23 13:31:53 -0300

    feat: create the query to normalize the unit challenges to check if all are completed

--- lingo/db/queries.ts
+++ lingo/db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
-import { courses, userProgress } from "./schema";
+import { challenges, challengesProgress, courses, lessons, units, userProgress } from "./schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -17,6 +17,39 @@ export const getUserProgress = cache(async () => {
     },
   });
   return data;
+});
+
+export const getUnits = cache(async () => {
+  const userProgress = await getUserProgress();
+
+  if (!userProgress?.activeCourseId) {
+    return [];
+  }
+
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengesProgress: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  const normalizedData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return challenge.challengesProgress && challenge.challengesProgress.length > 0 && challenge.challengesProgress.every((progress) => progress.completed);
+      });
+      return { ...lesson, completed: allCompletedChallenges };
+    });
+    return { ...unit, lessons: lessonsWithCompletedStatus };
+  });
+  return normalizedData;
 });
 
 export const getCourses = cache(async () => {
