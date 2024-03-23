d52ed0b21c5035015c5a054e207d9fa278d993fd
Nélio Dias <jrneliodias@gmail.com>
2024-03-23 13:28:08 -0300

    feat: implement news seeds to database

--- lingo/scripts/seed.ts
+++ lingo/scripts/seed.ts
@@ -11,6 +11,11 @@ const main = async () => {
     console.log("Seeding database");
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengesOptions);
+    await db.delete(schema.challengesProgress);
 
     await db.insert(schema.courses).values([
       {
@@ -33,7 +38,74 @@ const main = async () => {
         title: "Croatian",
         imageSrc: "/hr.svg",
       },
+      {
+        id: 5,
+        title: "Portuguese",
+        imageSrc: "/br.svg",
+      },
     ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Learn the basics of Spanish",
+        order: 1,
+      },
+    ]);
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        title: "Nouns",
+        order: 1,
+      },
+      {
+        id: 2,
+        unitId: 1,
+        title: "Vebs",
+        order: 2,
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        question: 'Which one of these is the "the man"?',
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.challengesOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/robot.svg",
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+      },
+    ]);
+
     console.log("Seeding finished.");
   } catch (error) {
     throw new Error("Failed to seed the database");
