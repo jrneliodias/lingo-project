b42d6904b6262b20c5142251d1f12bc755cf7a7a
Nélio Dias <jrneliodias@gmail.com>
2024-03-23 23:23:51 -0300

    refactor: insert to normalized Data a early return if the lesson dont have challenges

--- lingo/db/queries.ts
+++ lingo/db/queries.ts
@@ -45,6 +45,9 @@ export const getUnits = cache(async () => {
   });
   const normalizedData = data.map((unit) => {
     const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      if (lesson.challenges.length === 0) {
+        return { ...lesson, completed: false };
+      }
       const allCompletedChallenges = lesson.challenges.every((challenge) => {
         return challenge.challengesProgress && challenge.challengesProgress.length > 0 && challenge.challengesProgress.every((progress) => progress.completed);
       });
