7909823788bd6302e41f078e488e2c678d8d984a
Nélio Dias <jrneliodias@gmail.com>
2024-03-26 22:29:03 -0300

    feat: create layout lesson component

---
+++ lingo/app/lesson/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+type LessonLayoutProps = {
+    children: React.ReactNode
+}
+
+const LessonLayout = ({ children }: LessonLayoutProps) => {
+    return (
+        <div className="flex flex-col h-full">
+            <div className="flex flex-col h-full w-full">
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default LessonLayout
