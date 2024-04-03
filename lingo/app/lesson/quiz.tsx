9db859412368788125f9f384cbd30db6f6bf5dcd
Nélio Dias <jrneliodias@gmail.com>
2024-04-03 16:00:41 -0300

    feat: Insert Challenge inside the quiz component

--- lingo/app/lesson/quiz.tsx
+++ lingo/app/lesson/quiz.tsx
@@ -3,6 +3,7 @@ import { challenges, challengeOptions, challengeProgress } from "@/db/schema";
 import { useState } from "react";
 import Header from "./header";
 import QuestionBubble from "./question-bubble";
+import Challenge from "./challenge";
 
 interface QuizProps {
     initialLessonId: number;
@@ -28,6 +29,7 @@ const Quiz = ({ initialPercentage, initialHearts, initialLessonChallenges, initi
     })
 
     const challenge = challenges[activeIndex]
+    const options = challenge?.challengeOptions ?? []
 
     const title = challenge.type === "ASSIST" ? "Select the correct meaning"
         : challenge.question
@@ -41,17 +43,23 @@ const Quiz = ({ initialPercentage, initialHearts, initialLessonChallenges, initi
                 hasActiveSubscription={!!userSubscription?.isActive}
             />
             <div className="flex-1">
-                <div className="h-full bg-rose-400 flex items-center justify-center">
-                    <div className=" lg:min-h-[350px] lg:w-[600px] w-full bg-yellow-500  px-6 lg:px-0 flex flex-col gap-y-12">
+                <div className="h-full flex items-center justify-center">
+                    <div className=" lg:min-h-[350px] lg:w-[600px] w-full   px-6 lg:px-0 flex flex-col gap-y-12">
                         <h1 className="text-lg lg:text-3xl  text-center lg:text-start  font-bold text-neutral-700 ">
                             {title}
                         </h1>
                         <div>
-                            {
-                                challenge.type === "SELECT" && (
-                                    <QuestionBubble question={challenge.question} />
+                            {challenge.type === "ASSIST" && (
+                                <QuestionBubble question={challenge.question} />
+                            )}
+                            <Challenge
+                                options={options}
+                                onSelect={() => { }}
+                                status="none"
+                                selectedOption={undefined}
+                                type={challenge.type}
 
-                                )}
+                            />
                         </div>
 
                     </div>
