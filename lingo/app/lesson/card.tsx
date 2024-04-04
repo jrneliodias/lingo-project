54a53a03acca961ce6dbaa14d3c2b529955f1b63
Nélio Dias <jrneliodias@gmail.com>
2024-04-03 23:52:35 -0300

    implement the useAudio and onClick hook to play the audio from each alternative

--- lingo/app/lesson/card.tsx
+++ lingo/app/lesson/card.tsx
@@ -1,6 +1,8 @@
 import { challenges } from "@/db/schema";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { useCallback } from "react";
+import { useAudio, useKey } from "react-use"
 
 type Props = {
     id: number | undefined
@@ -27,14 +29,28 @@ const Card = ({
     status,
     type
 }: Props) => {
+
+    const [audio, _, controls] = useAudio({ src: audioSrc || "" })
+
+    const handleClick = useCallback(() => {
+        if (disabled) return
+        controls.play()
+        onClick();
+
+    }, [disabled, onClick, controls])
+
+    useKey(shortcut, handleClick, {}, [handleClick])
     return (
-        <div className={cn("h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
-            selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
-            selected && status === "correct" && "border-green-300 bg-green-100 hover:bg-green-100",
-            selected && status === "wrong" && "border-rose-300 bg-rose-100 hover:bg-rose-100",
-            disabled && "pointer-events-none hover:bg-white",
-            type === "ASSIST" && "lg:p-3 w-full"
-        )}>
+        <div
+            onClick={handleClick}
+            className={cn("h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
+                selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
+                selected && status === "correct" && "border-green-300 bg-green-100 hover:bg-green-100",
+                selected && status === "wrong" && "border-rose-300 bg-rose-100 hover:bg-rose-100",
+                disabled && "pointer-events-none hover:bg-white",
+                type === "ASSIST" && "lg:p-3 w-full"
+            )}>
+            {audio}
             {imageSrc &&
                 (<div className="relative aspect-square  mb-4 max-h-[80px] lg:max-h-[150px] w-full">
 
