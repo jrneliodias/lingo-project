c486ca7e82e2f5b2dab9e01f52719b8ededb1354
Nélio Dias <jrneliodias@gmail.com>
2024-04-06 20:38:57 -0300

    feat: implement the logic to correct answer

--- lingo/app/lesson/quiz.tsx
+++ lingo/app/lesson/quiz.tsx
@@ -1,10 +1,12 @@
 "use client"
 import { challenges, challengeOptions, challengeProgress } from "@/db/schema";
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import Header from "./header";
 import QuestionBubble from "./question-bubble";
 import Challenge from "./challenge";
 import Footer from "./footer";
+import { upsertChallengeProgress } from "@/actions/challenge-progress";
+import { toast } from "sonner";
 
 interface QuizProps {
     initialLessonId: number;
@@ -19,7 +21,7 @@ interface QuizProps {
 
 }
 const Quiz = ({ initialPercentage, initialHearts, initialLessonChallenges, initialLessonId, userSubscription }: QuizProps) => {
-
+    const [peding, startTransition] = useTransition()
     const [hearts, setHearts] = useState(initialHearts)
     const [percentage, setPercentage] = useState(initialPercentage)
     const [challenges] = useState(initialLessonChallenges);
@@ -34,6 +36,53 @@ const Quiz = ({ initialPercentage, initialHearts, initialLessonChallenges, initi
     const challenge = challenges[activeIndex]
     const options = challenge?.challengeOptions ?? []
 
+    const onNext = () => {
+        setActiveIndex((current) => current + 1)
+    }
+
+    const onContinue = () => {
+        if (!selectedOption) return
+
+        if (status === "wrong") {
+            setStatus("none")
+            setSelectedOptions(undefined)
+            return
+        }
+
+        if (status === "correct") {
+            onNext()
+            setStatus("none")
+            setSelectedOptions(undefined)
+            return
+        }
+
+        const correctOption = options.find((option) => option.correct)
+
+        if (!correctOption) return
+
+        if (correctOption.id === selectedOption) {
+            startTransition(() => {
+                upsertChallengeProgress(challenge.id)
+                    .then((response) => {
+                        if (response?.error === "hearts") {
+                            console.error("Missing hearts.")
+                            return
+                        }
+
+                        setStatus("correct")
+                        setPercentage((prev) => prev + 100 / challenges.length)
+                        if (initialPercentage === 100) {
+                            setHearts((prev) => Math.min(prev + 1, 5))
+                        }
+                    })
+                    .catch(() => toast.error("Something went wrong. Please try again."))
+            })
+        } else {
+            console.error("Incorrect option!")
+        }
+
+
+    }
     const onSelect = (id: number | undefined) => {
         if (status !== "none") return
         setSelectedOptions(id)
@@ -74,8 +123,8 @@ const Quiz = ({ initialPercentage, initialHearts, initialLessonChallenges, initi
             </div>
             <Footer
                 disabled={!selectedOption}
-                status={"completed" || status}
-                onCheck={() => { }}
+                status={status}
+                onCheck={onContinue}
 
             />
         </>
