40a3d4bfd617defdd03520757c9d065758cd335f
Nélio Dias <jrneliodias@gmail.com>
2024-04-13 23:30:50 -0300

    chore: setting up the stripe

--- lingo/middleware.ts
+++ lingo/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/webhooks/stripe"],
   // Routes that can always be accessed, and have
   // no authentication information
 });
