ee476ef06da7a8cb226866f4ddfb9e478858fcfb
Nélio Dias <jrneliodias@gmail.com>
2024-04-15 23:52:30 -0300

    feat: create the Admin App

---
+++ lingo/app/admin/page.tsx
@@ -0,0 +1,20 @@
+import { isAdmin } from "@/lib/admin";
+import dynamic from "next/dynamic"
+import { redirect } from "next/navigation";
+
+const App = dynamic(() => import("./app"), { ssr: false })
+const AdminPage = () => {
+
+
+    if (!isAdmin()) {
+        redirect("/")
+    }
+
+    return (
+        <div>
+            <App />
+        </div>
+    )
+}
+
+export default AdminPage
