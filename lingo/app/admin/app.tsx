0e445c963ccd6bc710f015261598a428a84dc8dd
Nélio Dias <jrneliodias@gmail.com>
2024-04-20 08:29:04 -0300

    feat: implement the component in Course dashboard

--- lingo/app/admin/app.tsx
+++ lingo/app/admin/app.tsx
@@ -1,7 +1,10 @@
 "use client"
 
-import { Admin, ListGuesser, Resource } from "react-admin"
+import { Admin, Resource } from "react-admin"
 import simpleRestProvider from "ra-data-simple-rest"
+import { CourseList } from "./course/list"
+import { CourseCreate } from "./course/create"
+import { CourseEdit } from "./course/edit"
 
 const dataProvider = simpleRestProvider("/api")
 
@@ -10,8 +13,10 @@ const App = () => {
         <Admin dataProvider={dataProvider}>
             <Resource
                 name="courses"
-                list={ListGuesser}
+                list={CourseList}
                 recordRepresentation={"title"}
+                create={CourseCreate}
+                edit={CourseEdit}
             />
         </Admin>
     )
