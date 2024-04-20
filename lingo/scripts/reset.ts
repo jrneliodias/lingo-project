9c15ae05134e2a8f3368c4e1e8860cdab23b44bc
Nélio Dias <jrneliodias@gmail.com>
2024-04-20 08:28:41 -0300

    feat: create the reset script

---
+++ lingo/scripts/reset.ts
@@ -0,0 +1,27 @@
+import { neon } from "@neondatabase/serverless";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log("Seeding database");
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
+    await db.delete(schema.userSubscription);
+
+    console.log("Reseting finished.");
+  } catch (error) {
+    throw new Error("Failed to reset the database");
+  }
+};
+
+main();
