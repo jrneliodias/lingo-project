92f330647c7527901d5150a2116dca5b4471e17e
Nélio Dias <jrneliodias@gmail.com>
2024-04-20 15:34:09 -0300

    feat: Create and implement the lesson admin components

--- lingo/app/admin/app.tsx
+++ lingo/app/admin/app.tsx
@@ -2,12 +2,18 @@
 
 import { Admin, Resource } from "react-admin"
 import simpleRestProvider from "ra-data-simple-rest"
+
 import { CourseList } from "./course/list"
 import { CourseCreate } from "./course/create"
 import { CourseEdit } from "./course/edit"
+
 import { UnitList } from "./unit/list"
 import { UnitCreate } from "./unit/create"
 import { UnitEdit } from "./unit/edit"
+
+import { LessonList } from "./lesson/list"
+import { LessonCreate } from "./lesson/create"
+import { LessonEdit } from "./lesson/edit"
 
 const dataProvider = simpleRestProvider("/api")
 
@@ -27,6 +33,13 @@ const App = () => {
                 recordRepresentation={"title"}
                 create={UnitCreate}
                 edit={UnitEdit}
+            />
+            <Resource
+                name="lessons"
+                list={LessonList}
+                recordRepresentation={"title"}
+                create={LessonCreate}
+                edit={LessonEdit}
             />
         </Admin>
     )
