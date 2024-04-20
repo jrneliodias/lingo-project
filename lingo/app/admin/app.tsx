41e3ab74fad68afaacdcd786d38ce14d001d0128
Nélio Dias <jrneliodias@gmail.com>
2024-04-20 15:10:22 -0300

    Add Unit in Admin App

--- lingo/app/admin/app.tsx
+++ lingo/app/admin/app.tsx
@@ -5,6 +5,9 @@ import simpleRestProvider from "ra-data-simple-rest"
 import { CourseList } from "./course/list"
 import { CourseCreate } from "./course/create"
 import { CourseEdit } from "./course/edit"
+import { UnitList } from "./unit/list"
+import { UnitCreate } from "./unit/create"
+import { UnitEdit } from "./unit/edit"
 
 const dataProvider = simpleRestProvider("/api")
 
@@ -17,6 +20,13 @@ const App = () => {
                 recordRepresentation={"title"}
                 create={CourseCreate}
                 edit={CourseEdit}
+            />
+            <Resource
+                name="units"
+                list={UnitList}
+                recordRepresentation={"title"}
+                create={UnitCreate}
+                edit={UnitEdit}
             />
         </Admin>
     )
